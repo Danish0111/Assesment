28c0b33007b98fbe8b2d6e25cf7f966967fe8ce0
Danish0111 <164894540+Danish0111@users.noreply.github.com>
2025-01-31 13:56:55 +0530

    changes

--- my-project/src/Components/Faqs.jsx
+++ my-project/src/Components/Faqs.jsx
@@ -13,68 +13,68 @@ const Faqs = () => {
                 </h1>
                 <div className='flex flex-col gap-4' id="accordion-collapse" data-accordion="collapse">
                     <h2 className='accordian_heading' id="accordion-collapse-heading-1">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
                             <span>Can I recover deleted files from desktop with this software?</span>
-                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
-                    <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-                        <div class="p-5 border border-gray-200">
-                            <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae culpa veniam dolore earum doloremque quibusdam, velit sequi quia asperiores, rerum minima. Id, optio itaque!</p>
+                    <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+                        <div className="p-5 border border-gray-200">
+                            <p className="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae culpa veniam dolore earum doloremque quibusdam, velit sequi quia asperiores, rerum minima. Id, optio itaque!</p>
                         </div>
                     </div>
                     <h2 className='accordian_heading' id="accordion-collapse-heading-2">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
                             <span>Can I recover deleted files from desktop with this software?</span>
-                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
-                    <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-                        <div class="p-5 border border-gray-200">
-                            <p class="mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet ipsa placeat, iste cupiditate expedita corrupti possimus eaque libero repellendus, perspiciatis nemo deleniti accusantium itaque.</p>
+                    <div id="accordion-collapse-body-2" className="hidden" aria-labelledby="accordion-collapse-heading-2">
+                        <div className="p-5 border border-gray-200">
+                            <p className="mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet ipsa placeat, iste cupiditate expedita corrupti possimus eaque libero repellendus, perspiciatis nemo deleniti accusantium itaque.</p>
                         </div>
                     </div>
                     <h2 className='accordian_heading' id="accordion-collapse-heading-3">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
                             <span>Can I recover deleted files from desktop with this software?</span>
-                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
-                    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-                        <div class="p-5 border border-gray-200">
-                            <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
+                    <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+                        <div className="p-5 border border-gray-200">
+                            <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
                         </div>
                     </div>
                     <h2 className='accordian_heading' id="accordion-collapse-heading-3">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
                             <span>Can I recover deleted files from desktop with this software?</span>
-                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
-                    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-                        <div class="p-5 border border-gray-200">
-                            <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
+                    <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+                        <div className="p-5 border border-gray-200">
+                            <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
                         </div>
                     </div>
                     <h2 className='accordian_heading' id="accordion-collapse-heading-3">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium shadow-md border border-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
                             <span>Can I recover deleted files from desktop with this software?</span>
-                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
-                    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-                        <div class="p-5 border border-gray-200">
-                            <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
+                    <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+                        <div className="p-5 border border-gray-200">
+                            <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptatum praesentium reiciendis rem rerum mollitia optio voluptates? Saepe praesentium reprehenderit ipsam alias, voluptate pariatur?</p>
                         </div>
                     </div>
                 </div>
